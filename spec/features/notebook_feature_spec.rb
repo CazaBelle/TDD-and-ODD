301d8c79023687605a5e3a3cf166b6a32a80fcff
Carol Gardiner <carolan.gardiner@gmail.com>
2019-02-16 13:44:17 +0000

    update config file

--- spec/features/notebook_feature_spec.rb
+++ spec/features/notebook_feature_spec.rb
@@ -1,5 +1,12 @@
 require 'notebook' 
 
 describe 'features' do 
-  before(:each)
+  before(:each) do 
+    @notebook = Notebook.new
+  end 
+
+  it 'user can add a note with some text' do 
+    expect(@notebook.add_note(''))
+  end 
+
 end 
